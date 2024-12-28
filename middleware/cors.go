ffaa25f8f220392a1046f40b5ba680044a5e4c3f
zikrykr <zikrykr@gmail.com>
2024-12-28 15:55:39 +0700

    chore(middleware): remove unnecessary header set

--- middleware/cors.go
+++ middleware/cors.go
@@ -11,7 +11,7 @@ func CORSMiddleware() gin.HandlerFunc {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, PATCH")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-App-Id, X-Client-Id, X-Client-Version, X-Mock-Data, accept, sec-ch-ua, sec-ch-ua-mobile, sec-ch-ua-platform, user-agent, x-device-id, x-page-location, referrer, ga-client-id, ga-session-id")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		c.Writer.Header().Set("Access-Control-Expose-Headers", "Content-Length")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 
